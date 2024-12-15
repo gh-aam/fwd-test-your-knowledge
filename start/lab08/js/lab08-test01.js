4a25e7d313278fe02b6c3390d21d67b02fb9b967
Abdullah Al Mamun <mamun777.aam@gmail.com>
2024-12-15 13:25:04 +0600

    Solution added

--- start/lab08/js/lab08-test01.js
+++ start/lab08/js/lab08-test01.js
@@ -1,2 +1,9 @@
 /* add code below this */
 
+let Bill = prompt("Enter total bill:");
+let iBill = Number(Bill);
+
+const tip_percentage = 0.10;
+let tip = iBill * tip_percentage;
+
+console.log(`For bill of $${Bill} the tip should be $${tip}`);
