64b1ff525b79e154a90eb44c46a87940120876ac
Abdullah Al Mamun <mamun777.aam@gmail.com>
2025-01-24 23:45:51 +0600

    Solution added

--- start/lab09/js/lab09-test01.js
+++ start/lab09/js/lab09-test01.js
@@ -1,2 +1,7 @@
-/* add code after this comment */
+document.getElementById("thumb-list").style.border = "solid 1px #CCCCCC";
+document.querySelector("#msg").value = document.querySelector("p").textContent;
 
+const images = document.querySelectorAll("img");
+for (let img of images) {
+    img.style.boxShadow = "6px 5px 20px 1px rgba(0, 0, 0, 0.22)";
+}
