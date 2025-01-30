af93205ff392d480ffba4ee7e1dcd386757f231a
Abdullah Al Mamun <mamun777.aam@gmail.com>
2025-01-30 17:50:35 +0600

    Solution added

--- start/lab09/js/lab09-test03.js
+++ start/lab09/js/lab09-test03.js
@@ -1,2 +1,10 @@
-/* add code after this comment */
-
+document.addEventListener('DOMContentLoaded', function () {
+    const panels = document.querySelectorAll('.panel');
+    
+    for (let panel of panels) {
+        panel.addEventListener('click', function () {
+            console.log('Clicked!');
+            this.classList.toggle('open');
+        });
+    }
+});
